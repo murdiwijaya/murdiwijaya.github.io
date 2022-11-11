6b2a4a2433e052088ccab8e42fe468f8a8c94a9e
Windu Purnomo <wpwindupurnomo@gmail.com>
2022-11-11 10:59:13 +0700

    tambah data keluarga

--- data.js
+++ data.js
@@ -1,7 +1,66 @@
 export const data = [
   { name: "Tardi Murdi", children: [] },
-  { name: "Odong Tardam", children: [] },
-  { name: "Engkus Kustaman", children: [] },
+  {
+    name: "Odong Tardam",
+    children: [
+      {
+        name: "Nining Sumartini",
+        children: [
+          { name: "Yoga Permana Wijaya" },
+          { name: "Laras Kusumawardani" },
+        ],
+      },
+      {
+        name: "Dodi Yulianto",
+        children: [
+          {
+            name: "Siska Yuliawati",
+            children: [{ name: "Dipo" }, { name: "???" }],
+          },
+          { name: "Yuki", children: [{ name: "???" }, { name: "???" }] },
+        ],
+      },
+      {
+        name: "Nunung Yuhani",
+        children: [
+          {
+            name: "Lusi Suciawati",
+            children: [{ name: "???" }, { name: "???" }],
+          },
+          { name: "Rian", children: [{ name: "???" }, { name: "???" }] },
+        ],
+      },
+      {
+        name: "Euis Hatriyani",
+        children: [{ name: "Putri" }, { name: "???" }],
+      },
+    ],
+  },
+  //   1. Acep Gunawan
+  // 2. Lilis Kusmayani
+  // 3. Novi
+  // 4. Hermawan
+  // 5. Nanang Setiawan
+  // 6. Anwar Suhendra
+  // 7. Dedi
+  // 8. Yolanda
+  // 9. Neni
+  // 10. Bambang
+  {
+    name: "Engkus Kustaman",
+    children: [
+      { name: "Acep Gunawan", children: [] },
+      { name: "Lilis Kusmayani", children: [] },
+      { name: "Novi", children: [] },
+      { name: "Hermawan", children: [] },
+      { name: "Nanang Setiawan", children: [] },
+      { name: "Anwar Suhendra", children: [] },
+      { name: "Dedi", children: [] },
+      { name: "Yolanda", children: [] },
+      { name: "Neni", children: [] },
+      { name: "Bambang", children: [] },
+    ],
+  },
   {
     name: "Edi Yanto",
     children: [
@@ -44,6 +103,29 @@ export const data = [
       },
     ],
   },
-  { name: "Eno" },
-  { name: "Entin" },
+  {
+    name: "Eno Rastab",
+    children: [
+      { name: "Iis Amorpatria", children: [{ name: "???" }, { name: "???" }] },
+      { name: "Susi Suprihatin", children: [{ name: "???" }, { name: "???" }] },
+      { name: "Imas", children: [{ name: "???" }, { name: "???" }] },
+    ],
+  },
+  {
+    name: "Entin Ruyatin",
+    children: [
+      {
+        name: "Yayan Kusmayadi",
+        children: [
+          { name: "Gema Andreanto", children: [{ name: "Zahran" }] },
+          { name: "Kevin Genta Buana" },
+        ],
+      },
+      {
+        name: "Yuyun Mahyunah",
+        children: [{ name: "Franky Fauzi Sugiri (alm)" }],
+      },
+      { name: "Erni", children: [{ name: "Tyara Aprilia" }] },
+    ],
+  },
 ];
